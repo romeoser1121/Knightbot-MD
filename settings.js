e87b61c09b5ef7841c2a6b161ae54a7bd04878f8
romeoser1121 <romeoser90@gmail.com>
2025-04-15 19:06:21 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'ꪶ𝐑𝐎𝐌𝐄𝐎 𝐒𝐄𝐑ꫂ',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "ꪶ𝐑𝐎𝐌𝐄𝐎 𝐒𝐄𝐑ꫂ",
+  botOwner: 'ꪶ𝐑𝐎𝐌𝐄𝐎 𝐒𝐄𝐑ꫂ', // Your name
+  ownerNumber: '918714387286', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
